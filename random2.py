d2c7bd60cdc31cd1b3b7aacb52c55a16167b5000
ndilnodji <ndilnojiblaise@gmail.com>
2023-02-01 13:02:30 +0100

    j'ai modifieé d'abord le nom du fichier etant donne qu'il y'a un conflit avec le nom du module random ,ensuite j'ai ajouté le nom de deux personne sdans la liste et enfin j'ai reduit la liste de groupe a 3 personnes

--- random2.py
+++ random2.py
@@ -1,5 +1,6 @@
 import random
-ma_list =["anne","aline","gros","eve","armand","yves","elv","allo","sonia","luc","marc","jules","kevin"]
+
+ma_list =["ndilnodji","innocent","anne","aline","gros","eve","armand","yves","elv","allo","sonia","luc","marc","jules","kevin"]
 #this will contain an occurence of our list
 maListOc = ma_list
 #this list will contain our random list
@@ -7,10 +8,12 @@ random_list = None
 groupe = 1
 #for each element in ma_list, we randome and put into our variable
 for i in ma_list:
+    
 #This condition will avoid errors when n is greater than 4
     if(len(ma_list)<=5):
         break
-    random_list = random.sample(ma_list, 6)
+     
+    random_list = random.sample(ma_list, 3)
     #then we remove data already randomize in our list, but the complexity is high for this little program
     for element in random_list:
         ma_list.remove(element)
