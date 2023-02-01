ee7a1723fe67296e45ee6bb3beffda9cfb41f942
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-02-01 13:09:18 +0100

    A function that generates teams randomly.

--- random.py
+++ random.py
@@ -1,22 +1,24 @@
 import random
+
+def team_generator (my_list) :
+    teams = []
+    teams_number = 0
+    if (len (my_list) / 5) % 5 == 0 :
+        teams_number = int (len (my_list) / 5)
+    else :
+        teams_number = int (len (my_list) / 5) + 1
+    for i in range (teams_number) :
+        if len (my_list) < 5 :
+            teams.append (my_list)
+            break
+        team = random.sample(my_list, 5)
+        teams.append (team)
+        for elem in team :
+            my_list.remove (elem)
+    for j in range (len (teams)) :
+        print (f"Groupe N°: {j + 1} \n{teams [j]}\n")
+        print ("_____________________________________________")
+
 ma_list =["anne","aline","gros","eve","armand","yves","elv","allo","sonia","luc","marc","jules","kevin"]
-#this will contain an occurence of our list
-maListOc = ma_list
-#this list will contain our random list
-random_list = None
-groupe = 1
-#for each element in ma_list, we randome and put into our variable
-for i in ma_list:
-#This condition will avoid errors when n is greater than 4
-    if(len(ma_list)<=5):
-        break
-    random_list = random.sample(ma_list, 6)
-    #then we remove data already randomize in our list, but the complexity is high for this little program
-    for element in random_list:
-        ma_list.remove(element)
-    print("Goupe N°:",groupe)
-    #and we finally print our randomized list
-    print(random_list)
-    print("______________________________")
-    groupe += 1
-print(maListOc)
+
+team_generator (ma_list)
