7e82be32ac5130626134efd35036397ed8fa4588
MBAINODJIMBAILAOSEVERIN <smbainodji@gmail.com>
2023-02-02 10:27:23 +0100

    ma part de contribution

--- random.py
+++ random.py
@@ -1,22 +1,25 @@
 import random
-ma_list =["anne","aline","gros","eve","armand","yves","elv","allo","sonia","luc","marc","jules","kevin"]
+ma_list =["SEV","KAGON","TAHI","TEMA","HAMZ","Hassane","Hypolite","Issa","Innocent","Julie","Lagré","Mahamat","Narcisse","Nil","Payang","Seraphin","Severin","jules","kevin"]
 #this will contain an occurence of our list
 maListOc = ma_list
 #this list will contain our random list
-random_list = None
+random_list = []
 groupe = 1
 #for each element in ma_list, we randome and put into our variable
 for i in ma_list:
 #This condition will avoid errors when n is greater than 4
-    if(len(ma_list)<=5):
+    if len(ma_list)<= 5 :
         break
-    random_list = random.sample(ma_list, 6)
+    random_list = random.sample(ma_list, 5)
     #then we remove data already randomize in our list, but the complexity is high for this little program
     for element in random_list:
-        ma_list.remove(element)
+            ma_list.remove(element)
     print("Goupe N°:",groupe)
-    #and we finally print our randomized list
+#and we finally print our randomized list
     print(random_list)
     print("______________________________")
-    groupe += 1
+#if ma_list :
+    groupe+=1
+SEVERIN=groupe
+print("Goupe N°:",SEVERIN)
 print(maListOc)
