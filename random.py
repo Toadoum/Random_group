fddd11192f622533b4a0617f6f5be2cf2466e4da
Mbaihondoum <mhondoum@gmail.com>
2023-02-09 08:40:16 +0100

    Ceci est le programme Random

--- random.py
+++ random.py
@@ -1,18 +1,23 @@
-from random import sample
-ma_liste = ["Hamidé", "Nil", "Hassan", "Ezechiel", "Succès", "Elie", "Bray", "Innocent", "Hamza", "Bertrand", "Issa", "Julie", "Exaucé", "Yannick", "Sevrin", "Hyppolite", "Seraphin", "Sakayo"]
-liste_randomise = []
+import random
+list =["Francis","Succès","Bray","Innocent","Hyppolite","Yassine","Severin","Ezechiel","Elie","Exocé","Honoré","Amédé","Nil","Julie","Bertrand","Saleh","Issa","Séraphin","Hamza","Hassan","Fourissou","Arsène","Narcisse",]
+#cela contiendra une occurrence de notre liste
+maListOc = list
+#cette liste contiendra notre liste aléatoire
+randomlist = None
 groupe = 1
-for i in ma_liste:
-    if len(ma_liste) <= 5:
+fin=5
+#pour chaque élément de ma_list, nous randome et mettons dans notre variable
+for i in list:
+#Cette condition évitera les erreurs lorsque n est supérieur à 4
+    if(len(list)<=fin):
         break
-    liste_randomise = sample(ma_liste, 6)
-    
-    for elem in liste_randomise:
-        ma_liste.remove(elem)
-    print("Groupe N° ", groupe)
-    print(liste_randomise)
-    print("----------------------------------------------------------")
-    groupe+=1
-if ma_liste:
-    print(ma_liste)
-        
+    randomlist = random.sample(list, 5)
+    #ensuite on supprime les données déjà randomisées dans notre liste, mais la complexité est élevée pour ce petit programme
+    for element in randomlist:
+        list.remove(element)
+    print("Groupe N°:",groupe)
+    #et nous imprimons enfin notre liste randomisée
+    print(randomlist)
+    print("_________________________________________")
+    groupe += 1
+print(maListOc)
